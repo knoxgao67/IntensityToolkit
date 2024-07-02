96bf2cf61d68d196d7decf3b5bba333820fa4787
knoxgao <knoxgao@163.com>
2024-07-03 07:45:22 +0800

    add intentsity segments

--- IntensitySegments.go
+++ IntensitySegments.go
@@ -1,8 +1,10 @@
 package IntentsityToolkit
 
 import (
-	"github.com/emirpasic/gods/v2/trees/redblacktree"
+	"fmt"
 	"math"
+
+	"github.com/emirpasic/gods/v2/trees/redblacktree"
 )
 
 // IntensitySegments 强度区间段
@@ -24,19 +26,130 @@ func NewIntensitySegments() *IntensitySegments {
 }
 
 func (s *IntensitySegments) Add(from, to, amount int64) {
-	panic("implement me")
+	s.baseOperate(from, to, amount, func(curAmount int64) int64 {
+		return curAmount + amount
+	})
 }
 
 func (s *IntensitySegments) Set(from, to, amount int64) {
-	panic("implement me")
+	s.baseOperate(from, to, amount, func(curAmount int64) int64 {
+		return amount
+	})
 }
 
 func (s *IntensitySegments) ToString() {
-	panic("implement me")
+	fmt.Println(s.GetAllValues())
+}
+
+func (s *IntensitySegments) GetAllValues() [][]int64 {
+	list := make([][]int64, 0, s.underlying.Size()) // 去除哨兵位置
+	if s.underlying.Size() <= 1 {
+		return [][]int64{}
+	}
+	iter := s.underlying.Iterator()
+	iter.Next() // 这里的目的是跳过哨兵
+	for iter.Next() {
+		list = append(list, []int64{iter.Value().From, iter.Value().Amount})
+	}
+	iter.Prev()
+	list = append(list, []int64{iter.Value().To, 0})
+	return list
 }
 
 // baseOperate 处理区间段更新，
-// amountUpdater 用于处理 重复区间段的更新逻辑，因为set和add的逻辑不同，固抽出这个function交由各自实现
+// amountUpdater 用于处理 重复区间段的更新逻辑，因为set和add的逻辑不同，固抽出这个function交由各自实现.
 func (s *IntensitySegments) baseOperate(from, to, amount int64, amountUpdater func(amount int64) int64) {
-	panic("implement me")
+	if from >= to {
+		panic("param error, from must small than to")
+	}
+
+	// 在tree里面所有小于等于from的keys中找到最大的那一个
+	curNode, ok := s.underlying.Floor(from)
+	if !ok { // 如果没有找到
+		// 因为插入了最小的节点，这里一定不能为false，如果出现false是代码的bug
+		panic("something error, there must have a small node")
+	}
+
+	iter := s.underlying.IteratorAt(curNode) // 迭代器
+
+	segOper := NewSegmentOperatorCache()
+
+	// curNode是前置区间段
+	if curNode.Value.To > from {
+		seg := curNode.Value
+		// 因为区间合并判断太过复杂，代码可读性很差，这里逻辑做了一定抽象处理
+		// 将oldSeg 和 newSeg进行合并，会得到三部分区间段，即重叠，重叠前，重叠后。对应下面添加的三个区间段
+		// 如果这三部分区间段部分不存在，NewSegment会返回nil，或许会进行过滤
+		segOper.Create(
+			NewSegment(seg.From, from, seg.Amount),                       // 重叠 区间前半部分
+			NewSegment(from, min(to, seg.To), amountUpdater(seg.Amount)), // 重叠 区间
+			NewSegment(to, seg.To, seg.Amount),                           // 重叠 区间后半部分
+		)
+		segOper.Delete(seg.From)
+	}
+
+	tmpFrom := max(from, curNode.Value.To)
+	// 遍历后面的区间段，直到后面的区间段的 from 大于 新区间段的 to
+	for iter.Next() {
+		seg := iter.Value()
+		if seg.From >= to {
+			break
+		}
+
+		segOper.Create(
+			NewSegment(tmpFrom, seg.From, amount),
+			NewSegment(seg.From, min(to, seg.To), amountUpdater(seg.Amount)),
+			NewSegment(to, seg.To, seg.Amount),
+		)
+		segOper.Delete(seg.From)
+
+		// 这里更新的意思是 tmpFrom以前的已经都处理了
+		tmpFrom = seg.To
+	}
+
+	if tmpFrom < to {
+		segOper.Create(NewSegment(tmpFrom, to, 0))
+	}
+
+	segOper.Merge()
+	for key := range segOper.deleteKeys {
+		s.underlying.Remove(key)
+	}
+	for _, item := range segOper.createSegs {
+		s.underlying.Put(item.From, item)
+	}
+
+	s.merge(from, to)
+}
+
+// merge 合并[prev] [from,to) [next]区间段
+// 这个和segmentOperate不同在于 它是在树内操作，主要是用于合并已经写入的区间.
+func (s *IntensitySegments) merge(from, to int64) {
+	startNode, ok := s.underlying.Ceiling(from)
+	if !ok {
+		// 如果找不到 则返回
+		return
+	}
+	iter := s.underlying.IteratorAt(startNode)
+	iter.Prev()
+	prev := iter.Value()
+	var needDeleteKeys []int64
+	for iter.Next() {
+		seg := iter.Value()
+		// should merge
+		if seg.From == prev.To && seg.Amount == prev.Amount {
+			needDeleteKeys = append(needDeleteKeys, seg.From)
+			prev.To = seg.To
+			continue
+		}
+		if seg.From > to {
+			break
+		}
+		prev = seg
+	}
+
+	// 将重复的segment删除
+	for _, key := range needDeleteKeys {
+		s.underlying.Remove(key)
+	}
 }
--- Segment.go
+++ Segment.go
@@ -1,9 +1,80 @@
 package IntentsityToolkit
 
 // Segment 表示区间段，左闭右开,[left,right)
-// amount表示区间段的值
+// amount表示区间段的值.
 type Segment struct {
 	From   int64
 	To     int64
 	Amount int64
+}
+
+func NewSegment(from, to, amount int64) *Segment {
+	if from >= to {
+		return nil
+	}
+	if amount == 0 {
+		return nil
+	}
+	return &Segment{
+		From:   from,
+		To:     to,
+		Amount: amount,
+	}
+}
+
+type SegmentOperatorCache struct {
+	createSegs []*Segment         // 标记后续需要create的区间
+	deleteKeys map[int64]struct{} // 标记后续需要delete的区间key
+}
+
+func NewSegmentOperatorCache() *SegmentOperatorCache {
+	return &SegmentOperatorCache{
+		createSegs: make([]*Segment, 0),
+		deleteKeys: make(map[int64]struct{}),
+	}
+}
+
+func (so *SegmentOperatorCache) Create(list ...*Segment) {
+	so.createSegs = append(so.createSegs, list...)
+}
+
+func (so *SegmentOperatorCache) Delete(list ...int64) {
+	for _, item := range list {
+		so.deleteKeys[item] = struct{}{}
+	}
+}
+
+// Merge 合并需要创建的区间段.
+func (so *SegmentOperatorCache) Merge() {
+	// 去掉nil，在Add和Set的时候我们可能会加入nil
+	segs := make([]*Segment, 0, len(so.createSegs))
+	for _, item := range so.createSegs {
+		if item == nil {
+			continue
+		}
+		segs = append(segs, item)
+	}
+
+	// merge
+	so.createSegs = segs
+	segs = make([]*Segment, 0, len(so.createSegs))
+	cur := so.createSegs[0]
+
+	for i := 1; i < len(so.createSegs); i++ {
+		seg := so.createSegs[i]
+		if cur.To >= seg.From && cur.Amount == seg.Amount {
+			cur.To = seg.To
+			// seg合并进cur，seg可以删除了
+			so.Delete(seg.From)
+		} else {
+			segs = append(segs, cur)
+			cur = seg
+		}
+	}
+
+	so.createSegs = segs
+
+	for _, item := range so.createSegs {
+		delete(so.deleteKeys, item.From)
+	}
 }
